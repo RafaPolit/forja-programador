30a67275a5097a6b3b00ae5bc548038a620d8ce3
RafaPolit <rafaelpolit@gmail.com>
2013-10-20 11:51:52 -0500

    Small refactor of script.

--- public/javascripts/form_utils.js
+++ public/javascripts/form_utils.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var cries = [];
+var input = document.getElementById('input');
+var input_display = document.getElementById('input-display');
+
 preload_previous_cries();
 
 function preload_previous_cries() {
-  var input = document.getElementById('input');
   if (input.value !== '') {
     cries = input.value.split('-');
   }
@@ -17,8 +19,6 @@ function remove_last_cry() { cries.pop(); update_input(); }
 function remove_all() { cries = []; update_input(); }
 
 function update_input() {
-  var input = document.getElementById('input');
-  var input_display = document.getElementById('input-display');
   input.value = cries.join('-');
   input_display.value = cries.join('-');
 }
