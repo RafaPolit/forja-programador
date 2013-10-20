0c02765ee35d7878e69f2c15ee9908e405ab9968
RafaPolit <rafaelpolit@gmail.com>
2013-10-20 11:50:05 -0500

    script refactor

--- public/javascripts/form_utils.js
+++ public/javascripts/form_utils.js
@@ -5,26 +5,16 @@ preload_previous_cries();
 
 function preload_previous_cries() {
   var input = document.getElementById('input');
-  cries = input.value.split('-');
-  if (cries.length === 1 && cries[0] === "") {
-    cries = [];
+  if (input.value !== '') {
+    cries = input.value.split('-');
   }
 }
 
-function add_cry(cry) {
-  cries.push(cry);
-  update_input();
-}
+function add_cry(cry) { cries.push(cry); update_input(); }
 
-function remove_last_cry() {
-  cries.pop();
-  update_input();
-}
+function remove_last_cry() { cries.pop(); update_input(); }
 
-function remove_all() {
-  cries = [];
-  update_input();
-}
+function remove_all() { cries = []; update_input(); }
 
 function update_input() {
   var input = document.getElementById('input');
